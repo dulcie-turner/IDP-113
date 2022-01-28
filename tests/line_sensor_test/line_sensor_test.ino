2d669ba456186e35ad0f37c1f13d2687d70def7a
Dulcie <77791388+dulcie-turner@users.noreply.github.com>
2022-01-28 09:45:03 +0000

    Create line_sensor_test.ino

---
+++ tests/line_sensor_test/line_sensor_test.ino
@@ -0,0 +1,20 @@
+int line_pins[] = {1, 2, 3, 4};
+int n_line_sensors = 4;
+bool line_reading[4];
+
+void setup() {
+  Serial.begin(9600);           // set up Serial library at 9600 bps
+
+  for (int i = 0; i < n_line_sensors; i++) {
+    pinMode(line_pins[i], INPUT);
+  }
+}
+
+void loop() {
+  for (int i = 0; i < n_line_sensors; i++) {
+    line_reading[i] = digitalRead(line_pins[i]);
+    Serial.println("Line sensor - " + String(i + 1) + " - Reading - " + String(line_reading[i]));
+    delay(1500);
+  }
+
+  }
