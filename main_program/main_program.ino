bd801cac5c3776f5739dd4767da87a9930aa6729
Dulcie <77791388+dulcie-turner@users.noreply.github.com>
2022-02-07 09:31:25 +0000

    moved over updated line follow code

--- main_program/main_program.ino
+++ main_program/main_program.ino
@@ -33,19 +33,12 @@ Adafruit_DCMotor *Motor2 = AFMS.getMotor(2);
 int line_pins[] = {1, 2, 3, 4};
 int n_line_sensors = 4;
 bool line_reading[4] = {0,0,0,0};
-bool invalid_reading1[] = {0,0,0,0};
-bool invalid_reading2[] = {1,0,1,0};
-bool invalid_reading3[] = {0,1,0,1};
-bool invalid_reading4[] = {1,1,0,1};
-bool invalid_reading5[] = {1,0,1,1};
-bool junction[] = {1,1,1,1};
-bool old_reading[4] = {0,0,0,0};
-bool new_reading[4] = {0,0,0,0};
 float line_follow_ratio = 0;
 int n_sensors_high = 0;
 int n_junction_readings = 0;
-int n_not_junction_readings = 0;
 int n_junctions = 0;
+int n_error_readings = 0;
+bool off_line = false;
 
 
 // ultrasonic definitions
@@ -370,53 +363,66 @@ void block_detection() {
 
 void decide_line_follow_speed() {
   n_sensors_high = 0;
-        if (line_reading != invalid_reading1) {
-          if (line_reading != invalid_reading2) {
-            if (line_reading != invalid_reading3) {
-              if (line_reading != invalid_reading4) {
-                if (line_reading != invalid_reading5) {
-                  float line_follow_ratio = 0;
-                  for (int i = 0; i < n_line_sensors; i++)  {
-                    line_follow_ratio += line_reading[i]*i;
-                    n_sensors_high += line_reading[i];
-                  }
-                  if (n_sensors_high == 0) {
-                    line_follow_ratio = 0;
-                  }
-                  else {
-                    line_follow_ratio = (line_follow_ratio/n_sensors_high) -1.5;
-                  }
-                  if (line_follow_ratio > 0) {
-                    left_speed = 250 - (line_follow_ratio * 50);
-                    right_speed = 250;
-                  }
-                  else {
-                    left_speed = 250;
-                    right_speed = 250 + (line_follow_ratio * 50);
-                  }
+  float line_follow_ratio = 0;
 
-                  bool is_junction = true;
-                  for (int i = 0; i < n_line_sensors; i++) {
-                    if (line_reading[i] != junction[i]){
-                       is_junction = false;
-                    }
-                  }
-                  if (is_junction) {
-                    n_junction_readings += 1;
-                  }
-                  else {
-                    n_junction_readings = 0;
-                  }
-                  if (n_junction_readings == 3){
-                    n_junctions += 1;                  }
+  // proportional control
+  for (int i = 0; i < n_line_sensors; i++)  {
+    line_follow_ratio += line_reading[i]*i;
+    n_sensors_high += line_reading[i];
+  }
+  if (n_sensors_high == 0) {
+    line_follow_ratio = 0;
+  }
+  else {
+    line_follow_ratio = (line_follow_ratio/n_sensors_high) -1.5;
+  }
 
-                  Serial.println(String(n_junctions));
+  // set motor speeds based on ratio
+  if (line_follow_ratio > 0) {
+    left_speed = 250 - (line_follow_ratio * 50);
+    right_speed = 250;
+  }
+  else {
+    left_speed = 250;
+    right_speed = 250 + (line_follow_ratio * 50);
+  }  
+  if (off_line) {
+    left_speed *= 2/3;
+    right_speed *= 2/3;
+  }
 
-                }
-              }
-            }
-          }
-        }
+  // if enough junction readings detected, robot is at a junction
+  if (n_sensors_high == 4) {
+    n_junction_readings += 1;
+  } else {
+    n_junction_readings = 0;
+  }
+  if (n_junction_readings == 3){
+    n_junctions += 1; 
+    n_junction_readings = 0;
+    Serial.println(String(n_junctions) + " junctions");  
+  }
+
+  // if enough error readings detected, robot has left line
+  if (n_sensors_high == 0) {
+    n_error_readings += 1;
+  } else {
+    if (off_line) {
+      // if line detected after reversing process, start going forward again
+      delay(5);
+      motors_forward();
+    }
+    n_error_readings = 0;
+    off_line = false;
+  }
+  if (n_error_readings == 30){
+    off_line = true;
+    n_error_readings = 0;
+
+    // start reversing
+    motors_backward();
+    Serial.println("lost line");
+  }
 }
 
 void line_following(){
